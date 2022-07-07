2457c7c09c5c2483896b02c9c0c849a36dc516c8
Farkhodoff21 <farkhodoffcodes@gmail.com>
2022-07-07 16:22:12 +1400

    initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+const video = document.querySelector('.video');
+const cameraButton = document.querySelector('.camera');
+const canvas = document.querySelector('.canvas');
+
+navigator.mediaDevices.getUserMedia({video: true})
+.then(stream => {
+    video.srcObject = stream;
+    video.play(); 
+})
